f95742d6ef016b2ab34a7a8e7a200e91819bbf5e
Vishal Patil <vishalpatil01@hotmail.com>
2020-02-17 19:16:35 +0530

    ADDED : Test Case For Validate Last Name

--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -15,4 +15,15 @@ public class UserRegistrationTest {
         boolean firstName =UserRegistration.validInputName("ameer13");
         Assert.assertFalse(firstName);
     }
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        boolean lastName = UserRegistration.validInputName("Patil");
+        Assert.assertTrue(lastName);
+    }
+
+    @Test
+    public void givenLastName_WhenImproper_ShouldReturnFalse() {
+        boolean lastName =UserRegistration.validInputName("PAtil7");
+        Assert.assertFalse(lastName);
+    }
 }
