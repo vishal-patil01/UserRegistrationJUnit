98adbf04bbdc37f92c51c42330a873d6dabc5b15
Vishal Patil <vishalpatil01@hotmail.com>
2020-02-18 19:20:36 +0530

    ADDED : Test Case To check All Provided Email Samples

---
+++ src/test/java/com/userregistration/EmailValidationSampleTest.java
@@ -0,0 +1,59 @@
+package com.userregistration;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class EmailValidationSampleTest {
+    private UserRegistration userRegistration;
+    private String emailId;
+    private boolean expectedResult;
+
+    public EmailValidationSampleTest(String emailId,boolean expectedResult) {
+        super();
+        this.emailId = emailId;
+        this.expectedResult = expectedResult;
+    }
+
+    @Before
+    public void initialize(){
+        userRegistration = new UserRegistration();
+    }
+
+    @Parameterized.Parameters
+    public static Collection data(){
+        return Arrays.asList(new Object[][] {
+                {"abc@yahoo.com",true},
+                {"abc-100@yahoo.com",true},
+                {"abc.100@yahoo.com",true},
+                {"abc111@abc.com",true},
+                {"abc-100@abc.net",true},
+                {"abc.100@abc.com.au",true},
+                {"abc@1.com",true},
+                {"abc@gmail.com.com",true},
+                {"abc-gmail.com",false},
+                {"abc@.com.my",false},
+                {"abc123@gmail.a",false},
+                {"abc123@.com",false},
+                {"abc123@.com.com",false},
+                {".abc@abc.com",false},
+                {"abc()*@gmail.com",false},
+                {"abc@%*.com",false},
+                {"abc..2002@gmail.com",false},
+                {"abc.@gmail.com",false},
+                {"abc@abc@gmail.com",false},
+                {"abc@gmail.com.1a",false},
+                {"abc@gmail.com.aa.au",false}});
+    }
+
+    @Test
+    public void testUserEmailId() {
+        Assert.assertEquals(expectedResult,userRegistration.validateEmailId(emailId));
+    }
+}
